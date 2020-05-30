2634494caeb5424ec6f6a9014259de77c2d82ae9
J.D. Zamfirescu <zamfire@gmail.com>
2020-05-30 00:42:35 -0700

    first cut of SIP data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Plot from 'react-plotly.js';
+// import logo from './logo.svg';
 import './App.css';
 import L from 'leaflet';
-import { Map, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
+import { Map, TileLayer, GeoJSON } from 'react-leaflet'
 import Papa from 'papaparse';
-import {addDays, format} from 'date-fns';
+import {addDays, format, differenceInCalendarDays, isSaturday} from 'date-fns';
 
 import * as topojson from 'topojson-client';
 
 import counties from './data/counties-10m.json';
+import cuebiq from './data/cmi-20200505.json';
+window.cuebiq = cuebiq;
 
 const COUNTIES_GEOJSON = topojson.feature(counties, counties.objects.counties)
   .features;
@@ -26,6 +29,8 @@ const ATTRIBUTION =
   '&copy; <a href="https://www.osm.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
   // '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
 const CANVAS =  L.canvas({ padding: 0.6 });
+
+const START_DATE = new Date(2020, 0, 22);
 
 class App extends React.Component {
   render() {
@@ -45,12 +50,12 @@ const BOUNDS_US = [
   [22.4, -64.96],
   [22.4, -127.51],
 ];
-const MAX_BOUNDS_US = [
-  [55.4, -54.96],
-  [55.4, -137.51],
-  [17.4, -54.96],
-  [17.4, -137.51],
-];
+// const MAX_BOUNDS_US = [
+//   [55.4, -54.96],
+//   [55.4, -137.51],
+//   [17.4, -54.96],
+//   [17.4, -137.51],
+// ];
 
 
 const BASE_FEATURE_STYLES = {
@@ -85,7 +90,13 @@ const DATALAYER_LABELS = new window.Map([
   ['compliance.cia_1', "Time since last left house"],
   ['compliance.cia_2', "Time since last observed < 6ft"],
   ['compliance.cia_3', "Time since last personal < 6ft"],
-  ['compliance.bts_1', "% others distancing, observed"]
+  ['compliance.bts_1', "% others distancing, observed"],
+  ['mobility.cmi', 'Mobility index'],
+  ['mobility.sip', '% < 350ft from home'],
+  ['mobility.l1m', '% < 1mi from home'],
+  ['mobility.l10m', '% < 10mi from home'],
+  ['mobility.observed_sip_date', 'Observed SIP date']
+  
 ]);
 
 const DATALAYER_LEGENDS = new window.Map([
@@ -95,7 +106,12 @@ const DATALAYER_LEGENDS = new window.Map([
   ['compliance.cia_1', {min: "< 1 day", max: "> 2 weeks", steps: 5, minR: 150, maxR: 250}],
   ['compliance.cia_2', {min: "< 1 day", max: "> 2 weeks", steps: 5, minR: 150, maxR: 250}],
   ['compliance.cia_3', {min: "< 1 day", max: "> 2 weeks", steps: 5, minR: 150, maxR: 250}],
-  ['compliance.bts_1', {min: "0%", max: "100%", steps: 4, minR: 100, maxR: 250}]
+  ['compliance.bts_1', {min: "0%", max: "100%", steps: 4, minR: 100, maxR: 250}],
+  ['mobility.cmi', {min:"0", max:"5", steps: 4, minR: 100, maxR: 250}],
+  ['mobility.sip', {min:"0%", max:"100%", steps: 4, minR: 100, maxR: 250}],
+  ['mobility.l1m', {min:"0%", max:"100%", steps: 4, minR: 100, maxR: 250}],
+  ['mobility.l10m', {min:"0%", max:"100%", steps: 4, minR: 100, maxR: 250}],
+  ['mobility.observed_sip_date', {min: "March 1", max: "May 1", steps:5, minR: 100, maxR: 250}]
 ]);
 
 // const DATALAYER_CONTROLS = new window.Map([
@@ -121,14 +137,13 @@ class VisMapLegend extends React.PureComponent {
 
 class VisMapDateSlider extends React.PureComponent {
   render() {
-    let {max, width, dayNumber} = this.props;
+    let {width, dayNumber, countyData: {maxDays, maxDaysMobility}, dataLayer} = this.props;
     let height = 10;
-    let startDate = new Date(2020, 0, 22);
-    let endDate = addDays(startDate, this.props.countyData.maxDays || 0);
+    let endDate = addDays(START_DATE, (dataLayer.startsWith('mobility.') ? maxDaysMobility : maxDays) || 0);
     return <div className="date-slider" style={{width: width, height: height + 20}}>
-        <span className="display" style={{position: "absolute", top: 0, width: "100%", "text-align": "center" }}>{format(addDays(startDate, dayNumber), 'M/d/yyyy')}</span>
-        <input type="range" style={{width: width}} min="0" max={this.props.countyData.maxDays || 0} step="1" value={dayNumber} onChange={event => this.props.setDayNumber(event.target.value)} />
-        <span className="leftIndex" style={{position: "absolute", top: height+10, left: 0}}>{format(startDate, 'M/d/yyyy')}</span>
+        <span className="display" style={{position: "absolute", top: 0, width: "100%", "text-align": "center" }}>{format(addDays(START_DATE, dayNumber), 'M/d/yyyy (EEEEEE)')}</span>
+        <input type="range" style={{width: width}} min="0" max={(dataLayer.startsWith('mobility.') ? maxDaysMobility : maxDays) || 0} step="1" value={dayNumber} onChange={event => this.props.setDayNumber(event.target.value)} />
+        <span className="leftIndex" style={{position: "absolute", top: height+10, left: 0}}>{format(START_DATE, 'M/d/yyyy')}</span>
         <span className="leftIndex" style={{position: "absolute", top: height+10, right: 0}}>{format(endDate, 'M/d/yyyy')}</span>
       </div>
   }
@@ -144,7 +159,8 @@ class VisMapMenu extends React.PureComponent {
     return (
       <div className="map-menu-container">
         <div className="legend-container">
-          {this.props.dataLayer.startsWith('cases.') && <VisMapDateSlider
+          {(this.props.dataLayer.startsWith('cases.') || this.props.dataLayer.startsWith('mobility.')) && <VisMapDateSlider
+            dataLayer={this.props.dataLayer}
             countyData={this.props.countyData}
             width={300}
             dayNumber={this.props.dayNumber}
@@ -170,7 +186,8 @@ class VisMap extends React.PureComponent {
   state = {
     countyData: {},
     dataLayer: "cases.growth",
-    dayNumber: 0
+    dayNumber: 0,
+    selectedRegions: new Set(["Alameda, California", "Santa Clara, California"])
   }
   
   transformComplianceValues(dataObject) {
@@ -219,6 +236,20 @@ class VisMap extends React.PureComponent {
       download: true,
       complete: this.processCaseResults
     });
+    // Papa.parse(window.location.href+'data/cmi-20200505.csv', {
+    //   download: true,
+    //   worker: true,
+    //   step: (results, parser) => console.log("parse step!"),
+    //   complete: this.processCuebiqResults
+    // })  
+    let maxDaysMobility = cuebiq.California.Alameda.cmi.length - 387;
+    this.setState(state => ({dayNumber: Math.min(state.dayNumber || 10000, maxDaysMobility), countyData: {...state.countyData, mobility: cuebiq, maxDaysMobility}}));
+  }
+  
+  componentDidUpdate() {
+    console.log("switching to data layer", this.state.dataLayer);
+    let [ns, k] = this.state.dataLayer.split('.');
+    console.log(this.state.countyData[ns], k);
   }
   
   transformCaseResults(headerIndices, dataObject) {
@@ -232,7 +263,7 @@ class VisMap extends React.PureComponent {
   }
   
   processCaseResults = (results, file) => {
-    console.log(results.data);
+    // console.log(results.data);
     var out = {};
     let header = results.data[0];
     let headerIndices = new window.Map(header.map((h, i) => [h, i]));
@@ -251,7 +282,7 @@ class VisMap extends React.PureComponent {
       if (out[state][county]) {
         console.log("weird, multiple case data for county", state, county);
       }
-      if (county == "New York") {
+      if (county === "New York") {
         console.log("new cases in NYC", entry.newCases);
       }
       out[state][county] = entry;
@@ -259,13 +290,68 @@ class VisMap extends React.PureComponent {
       maxNewCases = Math.max(maxNewCases, entry.newCases.reduce((a, b) => Math.max(a, b)));
       maxDays = Math.max(maxDays, entry.cases.length-1);
     }
-    // console.log("decorating with cases");
-    this.setState(state => ({dayNumber: maxDays, countyData: {...state.countyData, cases: out, maxCases, maxDays, maxNewCases}}));
+    console.log("got case data");
+    this.setState(state => ({dayNumber: Math.min(state.dayNumber || 10000, maxDays), countyData: {...state.countyData, cases: out, maxCases, maxDays, maxNewCases}}));
     // this.decorateCounties(out);
   }
   
+  isFeatureSelected = (regionData) => { 
+    return this.state.selectedRegions.has(`${regionData.name}, ${regionData.state}`);
+  }
+  setSelectedRegion = (regionData) => { 
+    console.log("set region", regionData);
+    const k = `${regionData.name}, ${regionData.state}`;
+    if (k) {
+      this.setState(state => {
+        const next = new Set(state.selectedRegions);
+        if (next.has(k)) {
+          next.delete(k)
+        } else {
+          next.add(k);
+        }
+        return {selectedRegions: next};
+      })
+    }
+  }
+  
+  onFeatureMouseOver = ({ layer }) => {
+    console.log("entering", layer.feature.properties);
+    const isSelected = this.isFeatureSelected(layer.feature.properties);
+    layer.openTooltip();
+    layer.setStyle({
+      color: '#ffffff',
+      weight: isSelected ? 3 : 2,
+      opacity: 1,
+    });
+  }
+
+  onFeatureMouseOut = ({ layer }) => {
+    console.log("leaving", layer.feature.properties);
+    const isSelected = this.isFeatureSelected(layer.feature.properties);
+    // layer.closeTooltip();
+    layer.setStyle({
+      ...BASE_FEATURE_STYLES,
+      weight: isSelected ? 3 : 1,
+      opacity: isSelected ? 1 : 0.25,
+    });
+  }
+
+  onFeatureClick = ({ layer }) => {
+    const properties = layer?.feature?.properties;
+    if (properties) {
+      // Hack to fix bug where click event is not propagating from Leaflet (say, to close map dropdown menu via useClickOutside) when clicking neighboring state
+      setTimeout(() => this.setSelectedRegion(properties));
+      // ReactGA.event({
+      //   category: 'Map',
+      //   action: 'County Click',
+      //   label: `${properties.regionName}, ${properties.state}`,
+      // });
+    }
+  }
+  
   geoJsonStyleForCounty = feature => {
-    let {id, properties: {name, state}} = feature;
+    const isSelected = this.isFeatureSelected(feature.properties);
+    let {properties: {name, state}} = feature;
     let [ns, k] = this.state.dataLayer.split('.');
 
     let data = this.state.countyData[ns]?.[state]?.[name];
@@ -285,43 +371,167 @@ class VisMap extends React.PureComponent {
       return l.reduce((p, c) => p+c,0)/l.length;
     }
 
-    if (ns == 'compliance') {
+    if (ns === 'compliance') {
       if (data) {
         r += avg(data.map(d => d[k]))*(maxR-minR)/steps;
       } else {
         return INVISIBLE_FEATURE_STYLES;
       }
-    } else if (ns == "cases") {
+    } else if (ns === "cases") {
       let i = this.state.dayNumber;
-      if (k == "counts") {
+      if (k === "counts") {
         let counts = data.cases[i];
         r += (Math.log(counts)/Math.log(this.state.countyData.maxCases))*(maxR-minR)
-      } else if (k == "new") {
+      } else if (k === "new") {
         let now = avg(data.newCases.slice(i-7, i));
         r += (Math.log(now < 1 ? 1 : now)/Math.log(this.state.countyData.maxNewCases))*(maxR-minR)
-      } else if (k == "growth") {
+      } else if (k === "growth") {
         let newCases = data.newCases;
-        let now = avg(newCases.slice(i-4, i));
-        let then = avg(newCases.slice(i-8, i-4));
-        if (name == "Santa Clara") {
+        let now = avg(newCases.slice(i-7, i));
+        let then = avg(newCases.slice(i-14, i-7));
+        if (name === "Santa Clara") {
           console.log("Santa Clara", now, then);
         }
         r = Math.min(Math.max(0, (minR + maxR) / 2 + (now-then) / 10 * (maxR-minR)), 255);
+      }
+    } else if (ns === "mobility") {
+      if (k === 'observed_sip_date') {
+        let maxIndex = data.cmi.reduce((iMax, x, i, arr) => i > 386 && x > arr[iMax] ? i : iMax, 0);
+        let minIndex = data.cmi.reduce((iMax, x, i, arr) => x !== null && i > 386+39 && x < arr[iMax] ? i : iMax, 0);
+        // console.log("max,min", maxIndex, minIndex, "->", data.cmi[maxIndex], data.cmi[minIndex]);
+        let halfwayThreshold = (data.cmi[maxIndex] + data.cmi[minIndex]) / 2;
+        let halfwayIndex = data.cmi.findIndex((x, i) => i > maxIndex && x < halfwayThreshold);
+        let daysSinceStart = halfwayIndex - 386 - 39; // since March 1 now
+        // console.log(name, state, "observed SIP on day", daysSinceStart, "as", data.cmi[maxIndex], "+", data.cmi[minIndex], "/ 2 =", halfwayThreshold, "- found", data.cmi[halfwayIndex], "on day", halfwayIndex);
+        r += daysSinceStart/61 * (maxR-minR)
+      } else {
+        let i = Number(this.state.dayNumber) + 386 // difference between 1-1-2019 and 1-22-2020, the start days of our data sets
+        let v = data[k][i];
+        if (k === 'l1m' || k === 'l10m') {
+          v += data.sip[i];
+        }
+        if (k === 'l10m') {
+          v += data.l1m[i];
+        }
+        let range = (k === 'cmi' ? 5 : 1)
+        r += (v / range) * (maxR-minR);
+        // console.log("drawing mobility data", v, "for", name, state, "on day", i);
       }
     }
 
     return {
       ...BASE_FEATURE_STYLES,
-      weight: 1,
+      color: "white",
+      weight: isSelected ? 3 : 1,
       fillOpacity: r < 0 ? 0 : 1,
-      opacity: 0.25,
+      opacity: isSelected ? 1 : 0.25,
       fillColor: `rgb(${r}, 200, 150)`
     };
   }
   
-  render() {
-    const position = [this.state.lat, this.state.lng];
+  extractData(ns, k, county, state, dayNumber) {
+    let data = this.state.countyData[ns]?.[state]?.[county];
+    let avg = l => {
+      l = l.filter(v => v !== undefined);
+      if (l.length === 0) {
+        return 0;
+      }
+      return l.reduce((p, c) => p+c,0)/l.length;
+    }
     
+    if (ns === "mobility") {
+      if (k === 'observed_sip_date') {
+        k = 'cmi';
+      }
+      let i = Number(dayNumber) + 386 // difference between 1-1-2019 and 1-22-2020, the start days of our data sets
+      let v = data[k][i];
+      if (k === 'l1m' || k === 'l10m') {
+        v += data.sip[i];
+      }
+      if (k === 'l10m') {
+        v += data.l1m[i];
+      }
+      let range = (k === 'cmi' ? 5 : 1)
+      return v / range;
+    }
+    if (ns === "cases") {
+      if (k === "growth") {
+        let i = dayNumber;
+        if (i <= 7) return;
+        let newCases = data.newCases;
+        let now = avg(newCases.slice(i-7, i));
+        let then = avg(newCases.slice(i-14, i-7));
+        return now/then;
+      }
+    }
+  }
+  
+  renderPlot() {
+    const daysSinceStart = differenceInCalendarDays(new Date(), new Date(2020, 0, 22));
+    const series = Array(daysSinceStart).fill(0).map((_, i) => i);
+    const dates = series.map(d => format(addDays(START_DATE, d), 'MMM d'));
+    const weekends = series.filter(d => isSaturday(addDays(START_DATE, d)));
+    // const position = [this.state.lat, this.state.lng];
+    let dataEntries = [];
+    for (let region of this.state.selectedRegions) {
+      let [ns, k] = this.state.dataLayer.split('.');
+      let [county, state] = region.split(',', 2).map(s => s.trim());
+      let data = this.state.countyData[ns]?.[state]?.[county];
+      if (data) {
+        let values = series.map(d => this.extractData(ns, k, county, state, d));
+        console.log("drawing, for", region, values);
+        dataEntries.push({
+          type: 'scatter',
+          x: dates,
+          y: values,
+          name: region          
+        })
+        // plots.push(<Plot
+        //   data={[{
+        //   }]}
+        //   layout={ {width: 500, height: 300, title: region} }
+        //   config={ {displayModeBar: false, fillFrame: true, scrollZoom: false} }
+        // />);
+      }
+    }
+    let layout = {width: 800, height: 350};
+    if (weekends.length > 0) {
+      layout.shapes = weekends.reduce((p, c, i) => {
+        if (p.length == 0 || p[p.length-1].x1 < c) {
+          p.push({
+            type: 'rect',
+            xref: 'x',
+            yref: 'paper',
+            x0: c,
+            y0: 0,
+            x1: c+1,
+            y1: 1,
+            fillcolor: '#d3d3d3',
+            opacity: 0.3,
+            line: {width: 0}
+          })
+        } else {
+          p[p.length-1].x1 = c+1;
+        }
+        return p;
+      }, []).map(d => ({
+        ...d, 
+        x0: format(addDays(START_DATE, d.x0), 'MMM d'),
+        x1: format(addDays(START_DATE, d.x1), 'MMM d')
+      }));
+      console.log("using layout shapes", layout.shapes);
+    }
+    if (dataEntries.length > 0) {
+      return <Plot
+          data={dataEntries}
+          config={ {displayModeBar: false, fillFrame: true, scrollZoom: false} }
+          layout={layout}
+        />
+    }    
+  }
+  
+  render() {
+    let plot = this.renderPlot();
     return (
       <div className="map-container">
         <Map bounds={BOUNDS_US} zoom={4} renderer={CANVAS}>
@@ -332,7 +542,10 @@ class VisMap extends React.PureComponent {
           <GeoJSON
             data={COUNTIES_GEOJSON}
             style={this.geoJsonStyleForCounty}
-          />}
+            onMouseOver={this.onFeatureMouseOver}
+            onMouseOut={this.onFeatureMouseOut}
+            onClick={this.onFeatureClick}
+          />
           {/* <Marker position={position}>
             <Popup>
               A pretty CSS3 popup.
@@ -346,6 +559,9 @@ class VisMap extends React.PureComponent {
           setDayNumber={n => this.setState({dayNumber: n})}
           setLayer={l => this.setState({dataLayer: l})}
         />
+        <div className="plot-div">
+          {plot}
+        </div>
       </div>
     )
   }
